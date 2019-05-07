3388582ae1c01132f7fe7e9dbf4dfe4f8a193e8f
abp4343 <50384794+abp4343@users.noreply.github.com>
2019-05-07 19:51:37 -0400

    Special Case
    Inclusion of Case with Imaginary Numbers

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -8,8 +8,13 @@ option_1 = 0
 option_2 = 0
 
 if ((b*b)-(4*a*c)) < 0:
-    print('answer involves imaginary numbers')
+    real_option_1_2 = -b/(2*a)
+    imaginary_option_1_2 = ((abs((b*b)-(4*a*c)))**(1/2))/(2*a)
+    print('\n\nimaginary roots found:')
+    print('x = ' + str(real_option_1_2) + ' + ' + str(imaginary_option_1_2) + \
+    	'i , ' + str(real_option_1_2) + ' - ' + str(imaginary_option_1_2) + 'i')
 else:
     option_1 = ((-b + (((b*b)-(4*a*c))**(1/2)))/(2*a))
     option_2 = ((-b - (((b*b)-(4*a*c))**(1/2)))/(2*a))
+    print('\n\nreal roots found:')
     print('x = ' + str(option_1) + ' , ' + str(option_2))
